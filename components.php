fe7fcec994aa22c8a4ff499707f9938e9b21d4a2
Ika45 <irinepopiashvili7@gmail.com>
2024-10-11 20:16:10 +0400

    add changes

--- components.php
+++ components.php
@@ -1,25 +1,35 @@
 <?php
 function doctors($doctors, $socials) {
-    foreach ($doctors as $doctor) {
-        echo '<div class="item">';
-            echo '<div class="box">';
-                echo '<div class="img-box">';
-                    echo '<img src="' .$doctor['image'] . '" alt="' .$doctor['name'] . '">';
-                echo '</div>';
-                
-                echo '<div class="detail-box">';
-                    echo '<h5>' . $doctor['name'] . '</h5>';
-                    echo '<h6>' .$doctor['educ'] . '</h6>';
-               
-                
-                    echo '<div class="social_box">';
-                    foreach ($socials as $social) {
-                        echo '<a href="' . $social['link'] . '">' . $social['icon'] . '</a>';
-                }
+    echo '<section class="team_section layout_padding">';
+        echo '<div class="container">';
+            echo '<div class="heading_container heading_center">';
+                echo '<h2>' ." Our".' '. '<span>'."Doctors". "<span>".'<h2>';
+            echo '</div>';
+            echo '<div class="carousel-wrap ">';
+                echo '<div class="owl-carousel team_carousel">';
+                    foreach ($doctors as $doctor) {
+                        echo '<div class="item">';
+                            echo '<div class="box">';
+                                echo '<div class="img-box">';
+                                    echo '<img src="' .$doctor['image'] . '" alt="' .$doctor['name'] . '">';
+                                echo '</div>';
+                                
+                                echo '<div class="detail-box">';
+                                    echo '<h5>' . $doctor['name'] . '</h5>';
+                                    echo '<h6>' .$doctor['educ'] . '</h6>';
+                            
+                                
+                                    echo '<div class="social_box">';
+                                    foreach ($socials as $social) {
+                                        echo '<a href="' . $social['link'] . '">' . $social['icon'] . '</a>';
+                                }
 
-                    echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    }
                 echo '</div>';
             echo '</div>';
         echo '</div>';
-    }
-};
+   '</section>';
--- index.php
+++ index.php
@@ -443,24 +443,12 @@ include('components.php');
 
   <!-- team section -->
 
-  <section class="team_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          Our <span>Doctors</span>
-        </h2>
-      </div>
-      <div class="carousel-wrap ">
-        <div class="owl-carousel team_carousel">
 
-          <?php
-            doctors($doctors, $socials)
-           ?>
+  <?php
+    doctors($doctors, $socials)
+  ?>
          
-        </div>
-      </div>
-    </div>
-  </section>
+   
 
   <!-- end team section -->
 
